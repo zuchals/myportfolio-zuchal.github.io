d3034d75e97215e7dc6fb49b9fc2adc159c37709
Zuchal <saputrozuchal@gmail.com>
2022-04-11 05:18:49 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#fb923c',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      fontFamily: {
+        inter: ['Inter'],
+      },
+    },
+  },
+  plugins: [],
+}
